37b6ee154dd3bad814a593ba875ddc8ff81f5fc4
doris <doris070714@gmail.com>
2024-01-11 15:29:11 +0800

    Initial Commit

---
+++ AnimatedGIF/AnimatedGIFApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedGIFApp.swift
+//  AnimatedGIF
+//
+//  Created by Doris Wen on 2024/1/11.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedGIFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
